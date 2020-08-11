8b22d90ecc129a053308719058a9a473c1be876e
Nhan <natenguyen51294@gmail.com>
2020-08-11 18:49:34 -0500

    ancestor done

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -15,23 +15,6 @@ class Queue():
         return len(self.queue)
 
 
-class Stack():
-    def __init__(self):
-        self.stack = []
-
-    def push(self, value):
-        self.stack.append(value)
-
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-
-    def size(self):
-        return len(self.stack)
-
-
 class Graph:
     def __init__(self):
         self.vertices = {}
@@ -47,21 +30,33 @@ class Graph:
             raise IndexError('Nonexistent vertex')
 
     def get_ancestor(self, vertex):
-        if vertex in self.vertices:
-            return self.vertices[vertex]
-        else:
-            raise IndexError('Nonexistent vertex')
-
-
-def build_graph(ancestors):
-    graph = Graph()
-    for parent, child in ancestors:
-
-        graph.add_ancestor(parent)
-        graph.add_ancestor(child)
-        graph.add_edge(parent, child)
-    return graph
+        return self.vertices[vertex]
 
 
 def earliest_ancestor(ancestors, starting_node):
-    graph = build_graph(ancestors)
+    graph = Graph()
+    for parent, child in ancestors:
+
+        graph.add_vertex(parent)
+        graph.add_vertex(child)
+        graph.add_edge(child, parent)
+
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_length = 1
+    aged_one = -1
+
+    while q.size():
+        path = q.dequeue()
+        vertex = path[-1]
+
+        if (len(path) == max_path_length and vertex < aged_one) or (len(path) > max_path_length):
+            aged_one = vertex
+            max_path_length = len(path)
+
+        for neighbor in graph.vertices[vertex]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+
+    return aged_one
